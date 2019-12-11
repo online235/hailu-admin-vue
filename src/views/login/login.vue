3f4895c3936b4ee3ce59544603c8e0bd122d3dd8
许智杰 <976778267@qq.com>
2019-12-11 09:19:55 +0800

    - [modify] 添加项目前缀

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -84,8 +84,8 @@ export default {
     }
     return {
       loginForm: {
-        username: '13825693085',
-        password: '123456'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [
