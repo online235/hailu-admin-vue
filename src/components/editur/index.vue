f549ef6d30abc203eab0f6845ef8c7d78475c100
许智杰 <976778267@qq.com>
2019-12-26 16:51:14 +0800

    - [format] 富文本编辑器只保留图片、撤销等按钮

--- src/components/editur/index.vue
+++ src/components/editur/index.vue
@@ -64,24 +64,24 @@ export default {
 
       // 配置菜单
       this.editor.customConfig.menus = [
-        'head', // 标题
-        'bold', // 粗体
-        'fontSize', // 字号
-        'fontName', // 字体
-        'italic', // 斜体
-        'underline', // 下划线
-        'strikeThrough', // 删除线
-        'foreColor', // 文字颜色
-        'backColor', // 背景颜色
-        'link', // 插入链接
-        'list', // 列表
-        'justify', // 对齐方式
-        'quote', // 引用
-        'emoticon', // 表情
+        // 'head', // 标题
+        // 'bold', // 粗体
+        // 'fontSize', // 字号
+        // 'fontName', // 字体
+        // 'italic', // 斜体
+        // 'underline', // 下划线
+        // 'strikeThrough', // 删除线
+        // 'foreColor', // 文字颜色
+        // 'backColor', // 背景颜色
+        // 'link', // 插入链接
+        // 'list', // 列表
+        // 'justify', // 对齐方式
+        // 'quote', // 引用
+        // 'emoticon', // 表情
         'image', // 插入图片
-        'table', // 表格
-        'video', // 插入视频
-        'code', // 插入代码
+        // 'table', // 表格
+        // 'video', // 插入视频
+        // 'code', // 插入代码
         'undo', // 撤销
         'redo', // 重复
         'fullscreen' // 全屏
